c79200d6a8713922559764db85525d8a85fdfe10
Praktykant <Praktykant@NAT-61.atcomputers.local>
2026-04-14 11:10:04 +0200

    Połączenie dwóch api (weryfikacja pesel + weryfikacja nip) oraz poprawienie błędów w kodzie weryfikacji peselu

---
+++ program/praktyki/Models/VerifyVies.cs
@@ -0,0 +1,13 @@
+﻿namespace praktyki_vies
+{
+    public class VerifyVies
+    {
+        public bool isValid { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string originalVatNumber { get; set; }
+        public string vatNumber { get; set; }
+        public string userError { get; set; }
+    }
+
+}
