239528761bee8e3304e489038f8d372d994062d5
Praktykant <Praktykant@NAT-61.atcomputers.local>
2026-04-28 12:13:56 +0200

    Dodanie folderu odpowiadającego za komendy w konsoli

--- program/praktyki/Program.cs
+++ program/praktyki/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using praktyki.Commands;
 using praktyki.Data;
 using praktyki.Models;
 using praktyki.Repositories;
 using praktyki.Services;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,49 +18,17 @@ builder.Services.AddOpenApi();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
 builder.Services.AddScoped<IClientService, ClientService>();
-builder.Services.AddHttpClient<praktyki.Services.ViesService>();
-builder.Services.AddScoped<praktyki.Services.VerifyPeselService>();
+builder.Services.AddScoped<VerifyPeselCommand>();
+builder.Services.AddScoped<VerifyPeselService>();
+builder.Services.AddHttpClient<ViesService>();
 builder.Services.AddHttpClient<WeatherService>();
-builder.Services.Configure<praktyki.Models.WeatherSettings>(
-builder.Configuration.GetSection("WeatherSettings"));
+builder.Services.Configure<praktyki.Models.WeatherSettings>(builder.Configuration.GetSection("WeatherSettings"));
+
 
 
 var app = builder.Build();
 
-if (args.Length > 0 && args[0] == "--verify-pesel")
-{
-    if (args.Length < 2)
-    {
-        Console.WriteLine("Błąd: Podaj PESEL");
-        return;
-    }
-
-    string pesel = args[1];
-
-    using (var scope = app.Services.CreateScope())
-    {
-        var peselService = scope.ServiceProvider.GetRequiredService<VerifyPeselService>();
-
-        bool isCheckSumOk = peselService.VerifyCheckSum(pesel);
-
-
-        if (isCheckSumOk)
-        {
-            var data = peselService.GetPeselData(pesel);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"PESEL: {pesel} jest POPRAWNY.");
-            Console.WriteLine($"Data urodzenia: {data.BirthDate}");
-            Console.WriteLine($"Płeć: {data.Gender}");
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"PESEL: {pesel} jest BŁĘDNY - suma kontrolna się nie zgadza");
-        }
-        Console.ResetColor();
-    }
-    return;
-}
+if (app.HandleConsoleCommand(args)) return;
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
